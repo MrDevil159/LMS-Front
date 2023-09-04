e53c59240e25c1e3ca9016bf5fe7dd4caacccde5
Mehfooz Ahmed <mehfoozmaaz@gmail.com>
2023-09-04 13:47:25 +0530

    Update

--- src/app/shared/guards/is-admin.guard.ts
+++ src/app/shared/guards/is-admin.guard.ts
@@ -2,14 +2,20 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-export const isAdminGuard: CanActivateFn = (route, state) => {
+export const isAdminGuard: CanActivateFn = async (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if(authService.isAdmin()) {
-    return true;
-  } else {
-    return router.createUrlTree(['/leaves']);
-    // return false;
+  try {
+    const isAdmin = await authService.isAdminGuardCheck();
+    if (isAdmin) {
+      return true; 
+    } else {
+      router.navigate(['/leaves']); 
+      return false;
+    }
+  } catch (error) {
+    console.error('Error checking admin status:', error);
+    return false; 
   }
 };
